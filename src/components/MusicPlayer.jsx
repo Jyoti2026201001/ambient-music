606e22fbcc97ff9f4555f04dc500a083d95934aa
Jyoti2026201001 <jyoti.pal@students.iiit.ac.in>
2026-08-15 23:36:50 +0530

    Create music player component

---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,16 @@
+const MusicPlayer =()=>
+{
+    return(<div className="music-player">
+      <h3>Now Playing</h3>
+
+      <p>Ambient Music</p>
+
+      <div className="controls">
+        <button>⏮</button>
+        <button>▶</button>
+        <button>⏭</button>
+      </div>
+    </div>);
+}
+
+export default MusicPlayer;
