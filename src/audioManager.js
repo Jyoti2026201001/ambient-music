5ea7d8f5abc6ab11b524175b35d38158bea30539
Jyoti2026201001 <jyoti.pal@students.iiit.ac.in>
2026-08-15 22:18:39 +0530

    Added functionality of audio

--- src/audioManager.js
+++ src/audioManager.js
@@ -1,16 +1,42 @@
 import { Howl } from "howler";
 
 let currentSound = null;
+let currentIndex = 0;
 
-export function playSong(file) {
+let currentSound = null;
+
+const songs = [
+  "/music/song1.mp3",
+  "/music/song2.mp3",
+  "/music/song3.mp3",
+];
+
+export function playSong(index) {
   if (currentSound) {
     currentSound.stop();
   }
 
   currentSound = new Howl({
-    src: [file],
+    src: [songs[index]],
     html5: true,
+    onend: () => { playNextSong(); },
   });
-
+  
   currentSound.play();
+}
+
+export function playNextSong() {
+  currentIndex = (currentIndex + 1) % songs.length;
+  playSong(currentIndex);
+}
+
+export function playPreviousSong() {
+  currentIndex = (currentIndex - 1 + songs.length) % songs.length;
+  playSong(currentIndex);
+}
+
+export function stopSong() {
+  if (currentSound) {
+    currentSound.stop();
+  }
 }
